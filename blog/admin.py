2a59e5113588b7e9f5f6b33ca1dcc1f7208a24ca
admin <chrm1031@gmail.com>
2020-04-22 07:45:55 +0900

    add the comment functions.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 # PostモデルをAdminページ（管理画面）上で見えるようにするため、
 # admin.site.register(Post)でモデルを登録する必要がある
 admin.site.register(Post)
+admin.site.register(Comment)
---
+++ mysite/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
+
+
+def logout_view(request):
+    logout(request)
+    return redirect()
